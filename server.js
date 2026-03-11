acacbda2e9600e7f09eae380ddea6054bb2a8b98
samankumar85006-ai <samankumar85006@gmail.com>
2026-03-11 02:18:29 -0700

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require("express");
 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
